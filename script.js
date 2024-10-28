f347158ea6257bd25594a0b5536b28f11fe1635a
booklandkz <sultanmaraluly@gmail.com>
2024-10-28 15:29:07 +0500

    Кітаптарды онлайн оқы

---
+++ script.js
@@ -0,0 +1,5 @@
+function openMessage() {
+    var newWindow = window.open("", "_blank");
+    newWindow.document.write("<html><head><title>Сайт өңделуде</title></head><body style='text-align:center; background-color: black; color: white;'><h1>Сайт қайта жасалуда</h1><p>Жақын арада бұл батырмалар жұмыс жасайды.<br>Түсіністікпен қарауларыңызды сұраймын!</p></body></html>");
+    newWindow.document.close();
+}
