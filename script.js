7c71d5f9b65dbbefb3633ab6a16b58aac5b789d9
booklandkz <sultanmaraluly@gmail.com>
2024-10-30 17:38:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function openMessage() {
     newWindow.document.write("<html><head><title>Сайт өңделуде</title></head><body style='text-align:center; background-color: black; color: white;'><h1>Сайт қайта жасалуда</h1><p>Жақын арада бұл батырмалар жұмыс жасайды.<br>Түсіністікпен қарауларыңызды сұраймын!</p></body></html>");
     newWindow.document.close();
 }
+
+
+function goToAqqauyrsyn() {
+    window.location.href = 'aqqauyrsyn.html';
+}
